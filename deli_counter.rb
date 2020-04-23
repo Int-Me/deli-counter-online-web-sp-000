49428f8501f28f0b076568cff6b760d394bca058
Roland McRae <mcraeroland@gmail.com>
2020-04-23 04:11:00 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,13 @@ def line(deli)
   if deli == []
     puts "The line is currently empty."
   else
-    list = ""
+    list = "The line is currently: "
+    ticknum = 1
+    deli.each { |name|
+      list << "#{ticknum}. #{name} "
+      puts list
+      ticknum++
+    }
   end
 end
 
